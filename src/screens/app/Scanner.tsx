c414dc3865200ac6468f9490bcbc71147587ea9d
Hugo A. Villalta Mena <hugo.villalta.mena@cr.ey.com>
2023-05-16 16:40:51 -0600

    install coed scanner (not working)

--- src/screens/app/Scanner.tsx
+++ src/screens/app/Scanner.tsx
@@ -1,10 +1,12 @@
-import React, {useEffect, useState} from 'react';
+import {
+  runOnJS
+} from 'react-native-reanimated';
+import React, {useEffect, useState, useCallback} from 'react';
 import {Text, View, StyleSheet} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import Button from '../../components/shared/Button';
-import { Camera } from 'react-native-vision-camera';
-import { useCameraDevices } from 'react-native-vision-camera';
-import CameraObject from '../../components/camera/CameraObject'
+import { Camera , useFrameProcessor, useCameraDevices} from 'react-native-vision-camera';
+//import { scanBarcodes, BarcodeFormat } from 'vision-camera-code-scanner';
 
 const styles = StyleSheet.create({
   container: {
@@ -22,15 +24,35 @@ const Scanner = () => {
 
   const devices = useCameraDevices();
   const device = devices.back;
+
+  const onBarCodeDected = useCallback((detectedBarcodes: any) => {
+
+    console.log("detectedBarcodes -> ", detectedBarcodes);
+
+  }, []);
+
+  // const frameProcessor = useFrameProcessor((frame) => {
+  //   'worklet'
+  //   const detectedBarcodes = scanBarcodes(frame, [BarcodeFormat.QR_CODE], { checkInverted: true });
+  //     // runOnJS(onBarCodeDected)(detectedBarcodes)
+  //   console.log('detectedBarcodes', detectedBarcodes); // It works!!
+
+  // }, [onBarCodeDected]);
   
   return (
    <>
-    { device !== undefined &&
+    { device !== undefined ?
       <Camera
        style={StyleSheet.absoluteFill}
        device={device}
        isActive={true}
+       //frameProcessor={frameProcessor}
+       //frameProcessorFps={60}
       />
+      :
+      <SafeAreaView>
+        <Text>no camera found</Text>
+      </SafeAreaView>
     }
    </>
      
