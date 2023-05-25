c98f6c4268d389aa2688ccc415aa9e18c6235069
Hugo A. Villalta Mena <hugo.villalta.mena@cr.ey.com>
2023-05-25 10:39:27 -0600

    sample api call

--- src/screens/app/Home.tsx
+++ src/screens/app/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import Button from '../../components/shared/Button';
 
 const styles = StyleSheet.create({
   container: {
@@ -9,10 +10,28 @@ const styles = StyleSheet.create({
 });
 
 const Home = () => {
+  
+  
+  const handleClick = () => {
+
+    return fetch('https://reactnative.dev/movies.json')
+    .then(response => response.json())
+    .then(json => {
+      console.log(json.movies);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  }
+
+  
   return (
     <SafeAreaView>
       <View style={styles.container}>
         <Text> HOME </Text>
+
+        <Button title="Api call" handleClick={handleClick} />
+
       </View>
     </SafeAreaView>
   );
